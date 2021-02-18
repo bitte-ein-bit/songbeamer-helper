a3c71fa0bd11289385dc8c4414e7a54c6112677e
Jonathan Vogt <code@v-o-g-t.net>
2021-02-19 00:04:12 +0100

    current

--- cmd/churchtools.go
+++ cmd/churchtools.go
@@ -42,9 +42,9 @@ func uploadToChurchTools() {
 	// }
 	// songs = nil
 
-	songs = processSongbeamerSongs(songs)
+	_ = processSongbeamerSongs(songs)
 	log.Println("These are all songs known at the end of processing:")
-	log.Println(songs)
+	// log.Println(songs)
 	// id := churchtools.AddSong("test1", "jonathan", "", "", "", "", "")
 	// id := 72
 	// log.Println(id)
@@ -115,151 +115,171 @@ func processSongbeamerSongs(songs map[string]churchtools.Song) map[string]church
 	}
 
 	for _, file := range files {
-		if filepath.Ext(file.Name()) == ".sng" {
-			fullpath := filepath.Join(path, file.Name())
-			song := songbeamer.Song{}
-			song.LoadFromFile(fullpath)
-			log.Printf("Working on %s", song.Title)
-			if song.ID != "" {
-				// Song has an ID, so it should exists in ChurchTools
-				a := song.ExtractArrangementFromFilename()
-				if a == "" {
-					a = "Standard-Arrangement"
-				}
-				log.Printf("Song has CT ID of %d, arrangement by Filename is %s, arrangement by ID is %s", song.ChurchToolsID, a, song.ChurchToolsArrangement)
-
-				ctSong := filterSongs(songs, "ID", song.ChurchToolsID)
-				if ctSong.ID == 0 {
-					song.ID = ""
-					song.Save()
-					song.FixFilename()
-					if err != nil {
-						log.Printf("Cannot fix filename %s", err)
-						song.MoveToDuplicates(duplicates)
-					}
-					log.Printf("CT Song ID can't be found, resetting ID and skip")
-					continue
-				}
-				arrangementID := 0
-				var arrangement churchtools.SongArrangement
-				for _, arrange := range ctSong.Arrangements {
-					if arrange.Bezeichnung == a {
-						log.Println("Arrangement found, checking if newer")
-						arrangementID = arrange.ID
-						arrangement = arrange
-					}
-				}
-				if arrangementID == 0 {
-					log.Printf("Seems song was renamed to new arrangement: %s", a)
-					arrangementID, err = ctSong.AddArrangement(a)
-					util.CheckForError(err)
-					arrangement = churchtools.SongArrangement{
-						ID:          arrangementID,
-						Bezeichnung: a,
-					}
-					err = song.UploadToArrangement(arrangement, duplicates)
-					if err != nil {
-						log.Fatalf("Error Uploading: %v", err)
-					}
-					continue
-				}
-				newer := true
-				APIFileToUpdate := *churchtools.NewSongAPIFile(song.Filename, arrangement.ID)
-
-				for _, file := range arrangement.Files {
-					log.Printf("Checking %s aka %s", file.Bezeichnung, file.Filename)
-					if file.Bezeichnung != song.GetFilenameWithoutArrangement() {
-						log.Printf("no match: %s != %s", file.Bezeichnung, song.GetFilenameWithoutArrangement())
-						continue
-					}
-					ctDate, _ := file.GetModificationDate()
-					sngDate, _ := song.GetModificationDate()
-					if sngDate.After(ctDate) {
-						log.Printf("CT is older: %v < %v", ctDate, sngDate)
-						APIFileToUpdate = file.ToAPIFile()
-						continue
-					}
-					log.Printf("CT is newer: %v >= %v", ctDate, sngDate)
-					newer = false
-				}
-				if !newer {
-					continue
-				}
-				log.Println("File needs to be updated on CT")
-				APIFileToUpdate.SetUploadName(song.GetFilenameWithoutArrangement())
-				APIFileToUpdate.LoadFromFile(song.Filename)
-				err = APIFileToUpdate.Save()
-				if err != nil {
-					log.Println(err)
-				}
-				continue
-			}
-			if song.CCLI != "" {
-				ctSong := filterSongs(songs, "CCLI", song.CCLI)
-				if ctSong.ID != 0 {
-					// found a matching song on ChurchTools
-					log.Println(ctSong)
-					song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
-					song.Title = ctSong.Bezeichnung
-					song.Save()
-					song.SetKeyOfArrangement(ctSong.GetDefaultArrangement())
-					// TODO arrangement erkennen
-					err := song.FixFilename()
-					if err != nil {
-						log.Printf("Cannot fix filename %s", err)
-						song.MoveToDuplicates(duplicates)
-					}
-
-					continue
-				}
-				id := churchtools.AddSong(song.Title, song.Author, song.Copyright, song.CCLI, song.KeyOfArrangement, "", "", cCLISongCat)
-				ctAPISong := churchtools.GetSong(id)
-				ctSong = ctAPISong.ToSong()
-				songs[fmt.Sprintf("%d", ctSong.ID)] = ctSong
-				err = song.UploadToArrangement(ctSong.GetDefaultArrangement(), duplicates)
-				if err != nil {
-					log.Fatalf("Error Uploading: %v", err)
-				}
-				continue
-			}
-			if song.Title != "" {
-				ctSong := filterSongs(songs, "Title", song.Title)
-
-				if ctSong.ID != 0 && ctSong.Author == song.Author {
-					log.Println(ctSong)
-					song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
-					song.Save()
-					song.SetKeyOfArrangement(ctSong.GetDefaultArrangement())
-					// TODO arrangement erkennen
-					err := song.FixFilename()
-					if err != nil {
-						log.Printf("Cannot fix filename %s", err)
-						song.MoveToDuplicates(duplicates)
-					}
-					continue
-				}
-
-				id := churchtools.AddSong(song.Title, song.Author, song.Copyright, song.CCLI, song.KeyOfArrangement, "", "", noCCLISongCat)
-				ctAPISong := churchtools.GetSong(id)
-				ctSong = ctAPISong.ToSong()
-				songs[fmt.Sprintf("%d", ctSong.ID)] = ctSong
-				err = song.UploadToArrangement(ctSong.GetDefaultArrangement(), duplicates)
-				if err != nil {
-					log.Fatalf("Error Uploading: %v", err)
-				}
-				continue
-			}
-			log.Fatal("Shouldn't reach this")
-			// err := song.FixFilename()
-			// if err != nil {
-			// 	log.Printf("Cannot fix filename %s", err)
-			// 	song.MoveToDuplicates(duplicates)
-			// }
-			// count++
-			// if count > 10 {
-			// 	break
-			// }
+		if filepath.Ext(file.Name()) != ".sng" {
+			continue
 		}
+		// time.Sleep(time.Millisecond * 100)
+		fullpath := filepath.Join(path, file.Name())
+		song := songbeamer.Song{}
+		song.LoadFromFile(fullpath)
+		log.Printf("Working on %s", song.Title)
+		if song.ID != "" {
+			// Song has an ID, so it should exists in ChurchTools
+			ctSong := filterSongs(songs, "ID", song.ChurchToolsID)
+			if ctSong.ID == 0 {
+				log.Printf("CT Song ID (%d) can't be found, resetting ID and skip", song.ChurchToolsID)
+				song.ID = ""
+				song.Save()
+				err = song.FixFilename()
+				if err != nil {
+					log.Printf("Cannot fix filename %s", err)
+					song.MoveToDuplicates(duplicates)
+				}
+				continue
+			}
+
+			a := song.ExtractArrangementFromFilename()
+			if a == "" {
+				a = "Standard-Arrangement"
+			}
+			log.Printf("Song has CT ID of %d, arrangement by Filename is %s, arrangement by ID is %s", song.ChurchToolsID, a, song.ChurchToolsArrangement)
+
+			var arrangement churchtools.SongArrangement
+			for _, arrange := range ctSong.Arrangements {
+				if arrange.Bezeichnung == a {
+					log.Println("Arrangement found, checking if newer")
+					arrangement = arrange
+				}
+			}
+			if arrangement.ID == 0 {
+				log.Printf("Seems song was renamed to new arrangement: %s", a)
+				arrangementID, err := ctSong.AddArrangement(a)
+				util.CheckForError(err)
+				arrangement = churchtools.SongArrangement{
+					ID:          arrangementID,
+					Bezeichnung: a,
+				}
+				err = song.UploadToArrangement(arrangement, duplicates)
+				if err != nil {
+					log.Fatalf("Error Uploading: %v", err)
+				}
+				song.SetID(ctSong.ID, arrangement)
+				err = song.FixFilename()
+				if err != nil {
+					log.Printf("Cannot fix filename %s", err)
+					song.MoveToDuplicates(duplicates)
+				}
+				continue
+			}
+			newer := true
+
+			err = song.FixFilename()
+			if err != nil {
+				log.Printf("Cannot fix filename %s", err)
+				song.MoveToDuplicates(duplicates)
+				continue
+			}
+			APIFileToUpdate := *churchtools.NewSongAPIFile(song.Filename, arrangement.ID)
+
+			for _, file := range arrangement.Files {
+				log.Printf("Checking %s aka %s", file.Bezeichnung, file.Filename)
+				if file.Bezeichnung != song.GetFilenameWithoutArrangement() {
+					log.Printf("no match: %s != %s", file.Bezeichnung, song.GetFilenameWithoutArrangement())
+					continue
+				}
+				ctDate, _ := file.GetModificationDate()
+				sngDate, _ := song.GetModificationDate()
+				if sngDate.After(ctDate) {
+					log.Printf("CT is older: %v < %v", ctDate, sngDate)
+					APIFileToUpdate = file.ToAPIFile()
+					continue
+				}
+				log.Printf("CT is newer: %v >= %v", ctDate, sngDate)
+				newer = false
+			}
+			if !newer {
+				continue
+			}
+			log.Println("File needs to be updated on CT")
+			APIFileToUpdate.SetUploadName(song.GetFilenameWithoutArrangement())
+			APIFileToUpdate.LoadFromFile(song.Filename)
+			err = APIFileToUpdate.Save()
+			if err != nil {
+				log.Println(err)
+			}
+			continue
+		}
+		if song.CCLI != "" {
+			ctSong := filterSongs(songs, "CCLI", song.CCLI)
+			if ctSong.ID != 0 {
+				// found a matching song on ChurchTools
+				log.Println(ctSong)
+				song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
+				song.Title = ctSong.Bezeichnung
+				song.Save()
+				song.SetKeyOfArrangement(ctSong.GetDefaultArrangement())
+				// TODO arrangement erkennen
+				err := song.FixFilename()
+				if err != nil {
+					log.Printf("Cannot fix filename %s", err)
+					song.MoveToDuplicates(duplicates)
+				}
+
+				continue
+			}
+			id := churchtools.AddSong(song.Title, song.Author, song.Copyright, song.CCLI, song.KeyOfArrangement, "", "", cCLISongCat)
+			ctAPISong := churchtools.GetSong(id)
+			ctSong = ctAPISong.ToSong()
+
+			song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
+			song.Save()
+			songs[fmt.Sprintf("%d", ctSong.ID)] = ctSong
+			err = song.UploadToArrangement(ctSong.GetDefaultArrangement(), duplicates)
+			if err != nil {
+				log.Fatalf("Error Uploading: %v", err)
+			}
+			continue
+		}
+		if song.Title != "" {
+			ctSong := filterSongs(songs, "Title", song.Title)
+
+			if ctSong.ID != 0 && ctSong.Author == song.Author {
+				log.Println(ctSong)
+				song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
+				song.Save()
+				song.SetKeyOfArrangement(ctSong.GetDefaultArrangement())
+				// TODO arrangement erkennen
+				err := song.FixFilename()
+				if err != nil {
+					log.Printf("Cannot fix filename %s", err)
+					song.MoveToDuplicates(duplicates)
+				}
+				continue
+			}
+
+			id := churchtools.AddSong(song.Title, song.Author, song.Copyright, song.CCLI, song.KeyOfArrangement, "", "", noCCLISongCat)
+			ctAPISong := churchtools.GetSong(id)
+			ctSong = ctAPISong.ToSong()
+			songs[fmt.Sprintf("%d", ctSong.ID)] = ctSong
+			song.SetID(ctSong.ID, ctSong.GetDefaultArrangement())
+			song.Save()
+			err = song.UploadToArrangement(ctSong.GetDefaultArrangement(), duplicates)
+			if err != nil {
+				log.Fatalf("Error Uploading: %v", err)
+			}
+			continue
+		}
+		song.MoveToDuplicates(duplicates)
+		log.Fatal("Shouldn't reach this")
+		// err := song.FixFilename()
+		// if err != nil {
+		// 	log.Printf("Cannot fix filename %s", err)
+		// 	song.MoveToDuplicates(duplicates)
+		// }
+		// count++
+		// if count > 10 {
+		// 	break
+		// }
 	}
 	return songs
 }
