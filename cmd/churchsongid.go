a7547d863b27a4747a0e009fae65343a17cd3a31
Jonathan Vogt <code@v-o-g-t.net>
2021-02-22 23:28:44 +0100

    less hard exit on running with old command

--- cmd/churchsongid.go
+++ cmd/churchsongid.go
@@ -15,6 +15,6 @@ var cmdChurchSongID = &cobra.Command{
 	Short: "Set ChurchSongID in all songs",
 	Long:  `Set ChurchSongID in all songs.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Fatal("No longer supported")
+		log.Println("No longer supported")
 	},
 }
